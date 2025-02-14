552dbf540d00a847ec70069efc810714b34d6be2
Sourabh Saini <sourabh61saini@gmail.com>
2025-02-14 17:42:43 +0530

    Added Level One Quiz Questions

--- my-react-app/src/Routes/Routes.tsx
+++ my-react-app/src/Routes/Routes.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router";
-import HomePage from "./Pages/HomePage";
-import LevelOneQuizPage from "./Pages/LevelOneQuizPage";
+import HomePage from "../Pages/HomePage";
+import LevelOneQuizPage from "../Pages/LevelOneQuizPage";
 
-const App = () => {
+const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
@@ -11,4 +11,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default AppRoutes;
