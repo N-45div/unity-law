de074dc3ae4547e08de231b08e1e69ce2c27d227
Prachi <developer.prachi@outlook.com>
2025-02-20 12:01:13 +0530

    Made the page mobile responsive

--- my-react-app/src/Pages/HomePage.tsx
+++ my-react-app/src/Pages/HomePage.tsx
@@ -37,7 +37,7 @@ const levelsData = [
   },
 ];
 
-const showNavBar = false;  // set to false to disable nav bar
+const showNavBar = true;  // set to false to disable nav bar
 
 const HomePage = () => {
   const levels = useMemo(() => levelsData, []);
@@ -71,7 +71,7 @@ const HomePage = () => {
       backgroundSize: "100% 100%",
     }}>
 
-
+{/* 
       <div className="flex justify-between ">
         <img
           src={iconImage}
@@ -84,17 +84,18 @@ const HomePage = () => {
           </button>
           <button className="w-auto bg-gradient-to-r from-[#FFF3B1] to-[#FFABF7]"><div style={buttonStyle}>PROGRESS</div></button>
         </div>
-      </div>
+      </div> */}
       {showNavBar && <Header />}
 
       <div className="max-w-7xl  py-15 text-center ">
 
         <p className=" text-[54px] font-bold text-white [font-family:'Roboto',sans-serif] mb-10">Lawyal Tech the intersection of Law and gaming </p>
         <button className="w-auto bg-gradient-to-r from-[#FFF3B1] to-[#FFABF7] m-2, p-2">
-          <div style={buttonStyle}>ABOUT US</div>
+          {/* <div style={buttonStyle}>ABOUT US</div> */}
+          <a href="#" style={buttonStyle}>ABOUT US</a>
         </button>
         <h3 className="text-3xl font-bold text-white [font-family:'Roboto',sans-serif] mt-25 mb-15">Play the Game Now !</h3>
-        <div className="flex gap-6 justify-center">
+        <div className="flex flex-col md:flex-row gap-6 justify-center">
           {levels.map(level => {
             return <LevelComponent key={level.title} levelName={level.title} />
           })}
