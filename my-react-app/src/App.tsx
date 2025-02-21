a62c6afb25a35bfa2555d8041fc265b789025022
Sourabh Saini <sourabh61saini@gmail.com>
2025-02-21 19:10:13 +0530

    Added Document Automation game

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router";
 import HomePage from "./Pages/HomePage";
 import LevelOneQuizPage from "./Pages/LevelOneQuizPage";
 import Level2 from "./Pages/Level2";
+import LevelTwoPart_Two from "./Pages/Level2_PartTwo";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/Level-One-Quiz" element={<LevelOneQuizPage />} />
-      <Route path="/Level-Two-Quiz" element={<Level2/>}/>
+      <Route path="/Level-Two-Part-One" element={<Level2/>}/>
+      <Route path="Level-Two-Part-Two" element={<LevelTwoPart_Two/>}/>
     </Routes>
   );
 };
