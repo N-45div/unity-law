f6f60545a59a0b985e3b93192da4e91a1cc8be30
Prachi <developer.prachi@outlook.com>
2025-02-25 18:00:36 +0530

    Added 4 pages of leveltwo. Made the pages as shown in the video. Added typing effect to the text. Add klara image and logo

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import HomePage from "./Pages/HomePage";
 import LevelOneQuizPage from "./Pages/LevelOneQuizPage";
+import LevelTwoQuiz from "./Pages/LevelTwo";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/Level-One-Quiz" element={<LevelOneQuizPage />} />
+      <Route path="/Level-Two-Quiz" element={<LevelTwoQuiz />} />
     </Routes>
   );
 };
--- my-react-app/src/Routes/Routes.tsx
+++ my-react-app/src/Routes/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import HomePage from "../Pages/HomePage";
 import LevelOneQuizPage from "../Pages/LevelOneQuizPage";
+import LevelTwo from "../Pages/LevelTwo";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/Level-One-Quiz" element={<LevelOneQuizPage />} />
+      <Route path="/Level-Two-Quiz" element={<LevelTwo />} />
     </Routes>
   );
 };
